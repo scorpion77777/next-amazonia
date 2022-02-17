4ab0890fdcf09a8a90e4c34cfee04b4349563b92
scorpion77777 <vvmartirosyan@gmail.com>
2022-02-16 20:32:40 -0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <ul>
+          <li>Product1</li>
+          <li>Product2</li>
+          <li>Product3</li>
+        </ul>
+      </div>
+    </Layout>
+  );
+}
